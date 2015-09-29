4e93042c6545d008376c15c232f8f325367cf682
German Lena <german.lena@gmail.com>
2015-09-29 17:44:38 -0300

    refactor charts + added color handling

--- standalone.js
+++ standalone.js
@@ -13,8 +13,7 @@ var old_define = global.define;
 global.define = undefined;
 
 var Auth0DasboardWidget = require('./');
-var Auth0AgeWidget = require('./lib/charts/age');
-var Auth0GenderWidget = require('./lib/charts/gender');
+var Auth0BasicChartWidget = require('./lib/charts/basic');
 var Auth0PieDataProcessor = require('./lib/data_processor/standard_pie');
 
 //restore define
@@ -23,12 +22,10 @@ global.define = old_define;
 // use amd or just throught to window object.
 if (typeof global.define == 'function' && global.define.amd) {
   global.define('auth0-dashboard-widget', function () { return Auth0DasboardWidget; });
-  global.define('auth0-age-widget', function () { return Auth0AgeWidget; });
-  global.define('auth0-gender-widget', function () { return Auth0GenderWidget; });
+  global.define('auth0-basic-chart-widget', function () { return Auth0BasicChartWidget; });
   global.define('auth0-pie-data-processor', function () { return Auth0PieDataProcessor; });
 } else if (global) {
   global.Auth0DasboardWidget = Auth0DasboardWidget;
-  global.Auth0AgeWidget = Auth0AgeWidget;
-  global.Auth0GenderWidget = Auth0GenderWidget;
+  global.Auth0BasicChartWidget = Auth0BasicChartWidget;
   global.Auth0PieDataProcessor = Auth0PieDataProcessor;
 }
